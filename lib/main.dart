dc72bd86fe29af550a91ca76659e6b1a32714d26
Mohsim <wasimzamanedu@gmail.com>
2023-06-14 22:21:55 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Alessa',
+      title: 'Alessa App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
